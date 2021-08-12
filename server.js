a006d5e0ba8857d0233f43f1cb83dc94518160d6
Chuks Ossai <ossaiwisdom48@gmail.com>
2021-08-12 12:26:04 +0100

    Implement middlewares

--- server.js
+++ server.js
@@ -1,7 +1,16 @@
+require('dotenv').config();
 const express = require('express');
+const morgan = require('morgan');
+const cors = require('cors');
+const mongoose = require('mongoose');
 const server = express();
 
 const authRoutes = require('./routes/auth');
+
+server.use(morgan('dev'));
+server.use(express.urlencoded({extended: true}));
+server.use(express.json());
+server.use(cors());
 
 server.use('/api/v1', authRoutes);
 
